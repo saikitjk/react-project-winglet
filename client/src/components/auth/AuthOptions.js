b98b0369604990f890022588b66d43de26f21508
JK <saikitjk@gmail.com>
2020-11-05 21:34:11 -0800

    created logout feature

--- client/src/components/auth/AuthOptions.js
+++ client/src/components/auth/AuthOptions.js
@@ -1,27 +1,46 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useHistory } from "react-router-dom";
+import UserContext from "../context/UserContext";
 import "./authOptionStyle.css";
 
 export default function AuthOptions() {
+  const { userData, setUserData } = useContext(UserContext);
+  console.log("here " + JSON.stringify(userData));
+
   const history = useHistory();
   const register = () => history.push("/register");
   const login = () => history.push("/login");
+  //remove user token if logout
+  const logout = () => {
+    setUserData({
+      token: undefined,
+      user: undefined,
+    });
+    localStorage.setItem("auth-token", "");
+  };
   return (
-    <div className="auth-option">
-      <button
-        type="button"
-        className="btn btn-outline-dark registerButton"
-        onClick={register}
-      >
-        Register
-      </button>
-      <button
-        type="button"
-        className="btn btn-outline-dark loginButton"
-        onClick={login}
-      >
-        Login
-      </button>
-    </div>
+    <nav className="auth-option">
+      {userData.user ? (
+        <button onClick={logout}>Logout</button>
+      ) : (
+        <>
+          <button
+            type="button"
+            className="btn btn-outline-dark registerButton"
+            onClick={register}
+          >
+            Sign Up
+          </button>
+
+          <button
+            type="button"
+            className="btn btn-outline-dark loginButton"
+            onClick={login}
+          >
+            Login
+          </button>
+        </>
+      )}
+    </nav>
   );
 }
