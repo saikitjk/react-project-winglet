52522812ba9a273a72d89bc5400f58df3dc080a5
JK <saikitjk@gmail.com>
2020-11-06 11:02:57 -0800

    created submit function and linked to the form. still need to fix it is not going to home page

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,18 +1,39 @@
 import React, { useContext, useState } from "react";
+import Axios from "axios";
 import { useHistory } from "react-router-dom";
 import "./registerStyle.css";
+import UserContext from "../context/UserContext";
 
 export default function Register() {
+  const [userName, setUserName] = useState();
   const [userEmail, setEmail] = useState();
   const [password, setPassword] = useState();
   const [confirmPassword, setConfirmPassword] = useState();
-  const [userName, setUserName] = useState();
+
+  const history = useHistory();
+  const { setUserData } = useContext(UserContext);
+
+  const submit = async (e) => {
+    e.prventDefault();
+    const newUser = { userName, userEmail, password, confirmPassword };
+    await Axios.post("http://localhost:8080/users/register", newUser);
+    const loginRes = await Axios.post("http://localhost:8080/users/login", {
+      userEmail,
+      password,
+    });
+    setUserData({
+      token: loginRes.data.token,
+      user: loginRes.data.user,
+    });
+    localStorage.setItem("auth-token", loginRes.data.token);
+    history.push("/");
+  };
 
   return (
     <>
       <div className="signUpPage">
         <h2>Sign Up</h2>
-        <form>
+        <form onSubmit={submit}>
           <div class="form-group">
             <label htmlFor="inputUsername">Username</label>
             <input
