faf6f0506f5f60283934ed8f6adb083aa77e4617
JK <saikitjk@gmail.com>
2020-11-17 16:26:51 -0800

    created template obj for a signle return data

--- client/src/components/yelp/Search/SearchResultSummary/OneResultObj.js
+++ client/src/components/yelp/Search/SearchResultSummary/OneResultObj.js
@@ -4,15 +4,21 @@ import "./oneResultObj.css";
 export default function OneResultObj() {
   return (
     <div className="oneResultOBJ">
-      <img src="http://via.placeholder.com/150" alt="business" />
-      <div clasName="busInfo">
+      <div className=" col-3 busPic">
+        <img src="http://via.placeholder.com/150" alt="business" />
+      </div>
+      <div className="col-6 busInfo">
         <h2 className="subtitle"> burger place</h2>
         <p>rating</p>
         <p>
-          $$ <span class="tag">Tag label</span>
+          $$ <span class="tag">burger</span> <span class="tag">fast food</span>
         </p>
       </div>
-      <p>address</p>
+      <div className=" col-3 busContact">
+        <p>111-222-3333</p>
+        <p>street</p>
+        <p>city</p>
+      </div>
     </div>
   );
 }
