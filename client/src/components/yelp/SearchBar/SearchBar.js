fa8aa39bda8e22d9bafa073ab66333b47a04a2c3
JK <saikitjk@gmail.com>
2020-11-17 17:28:16 -0800

    added submit function

--- client/src/components/yelp/SearchBar/SearchBar.js
+++ client/src/components/yelp/SearchBar/SearchBar.js
@@ -5,37 +5,44 @@ export default function SearchBar(props) {
   const [term, setTerm] = useState(props.term || "");
   const [location, setLocation] = useState(props.location || "");
 
+  function submit(e) {
+    e.preventDefault();
+    console.log(term, location);
+  }
+
   return (
     <div className="row searchRow">
-      <div className="field has-addons">
-        <div className="control">
-          <div className="button is-static">Search</div>
+      <form onSubmit={submit}>
+        <div className="field has-addons">
+          <div className="control">
+            <div className="button is-static">Search</div>
+          </div>
+          <div className="control">
+            <input
+              className="input"
+              type="text"
+              placeholder="Resturants, bars, shops.."
+              onChange={(e) => setTerm(e.target.value)}
+            />
+          </div>
+          <div className="control">
+            <div className="button is-static">Near</div>
+          </div>
+          <div className="control">
+            <input
+              className="input"
+              type="text"
+              placeholder="City"
+              onChange={(e) => setLocation(e.target.value)}
+            />
+          </div>
+          <button className="button searchButton">
+            <span className="icon is-small">
+              <i className="fas fa-search"></i>
+            </span>
+          </button>
         </div>
-        <div className="control">
-          <input
-            className="input"
-            type="text"
-            placeholder="Resturants, bars, shops.."
-            onChange={(e) => setTerm(e.target.value)}
-          />
-        </div>
-        <div className="control">
-          <div className="button is-static">Near</div>
-        </div>
-        <div className="control">
-          <input
-            className="input"
-            type="text"
-            placeholder="City"
-            onChange={(e) => setLocation(e.target.value)}
-          />
-        </div>
-        <button className="button searchButton">
-          <span className="icon is-small">
-            <i className="fas fa-search"></i>
-          </span>
-        </button>
-      </div>
+      </form>
     </div>
   );
 }
