133c0ade8204e00dcd02259437158eed8ccf7b3d
JK <saikitjk@gmail.com>
2020-11-22 23:54:55 -0800

    deploy 8

--- server.js
+++ server.js
@@ -5,12 +5,11 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
 const PORT = process.env.PORT || 8080;
-
-app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -18,22 +17,39 @@ if (process.env.NODE_ENV === "production") {
 
 // Define API routes here
 
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
-// mongoose
-
 mongoose.connect(
-  process.env.REACT_APP_mongoDBConnectionURL,
-  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+  process.env.MONGODB_URI ||
+    `mongodb+srv://${process.env.mongoDBConnectionURL}`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  },
   (err) => {
     if (err) throw err;
     console.log("MongoDB connection established.");
   }
 );
 
+// mongoose
+
+// mongoose.connect(
+//   process.env.REACT_APP_mongoDBConnectionURL,
+//   { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+//   (err) => {
+//     if (err) throw err;
+//     console.log("MongoDB connection established.");
+//   }
+// );
+
 //set routes
 
 app.use("/users", require("./routes/userRouter"));
+////
+app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
